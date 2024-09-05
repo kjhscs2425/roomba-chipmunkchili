27f76cda86fe4b572fe1f4681b0e8c65022053e3
chipmunkchili <stalcott2027@kehillahstudent.org>
2024-09-05 11:35:36 -0700

    move turtle, level 3

--- roomba_level3.py
+++ roomba_level3.py
@@ -9,14 +9,58 @@ from turtle import right, left, forward, backward, speed
 import room
 
 # Make the turtle go faster
-speed(7)
+speed(6)
 
 # Draw the Level 3 version of the room
-window = room.draw_room(level = 3)
+window = room.draw_room(level = 3, radius = 5)
 
 ###
 # Start your code here
- 
+forward(400)
+backward(40)
+right(90)
+forward(40)
+right(90)
+forward(320)
+left(90)
+forward(40)
+left(90)
+forward(320)
+right(90)
+forward(40)
+right(90)
+forward(320)
+backward(40)
+left(90)
+forward(40)
+left(90)
+forward(240)
+backward(120)
+right(90)
+forward(40)
+right(180)
+forward(240)
+left(90)
+forward(160)
+backward(320)
+right(90)
+forward(40)
+left(90)
+forward(320)
+right(90)
+forward(40)
+right(90)
+forward(320)
+backward(40)
+left(90)
+forward(40)
+left(90)
+forward(240)
+backward(120)
+right(90)
+forward(40)
+
+
  
  
 # End your code here
